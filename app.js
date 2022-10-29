d54bef37fe52cfa2e8d8c95fecfa35f4e015be3a
Sarang Sami <sarangsami75@gmail.com>
2022-10-30 01:06:19 +0330

    feat: :art: Read port from env

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fmmxuub.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8080);
+    app.listen(8080||process.env.PORT);
   })
   .catch((err) => {
     console.log(err);
